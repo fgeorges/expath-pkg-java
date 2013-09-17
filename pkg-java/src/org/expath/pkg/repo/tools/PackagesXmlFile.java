5852d7529d3b0414f7836d277b40c7c0d5ef2457
Florent Georges <fgeorges@dhcp-163-1-214-169.seh.ox.ac.uk>
2013-09-17 19:26:33 +0200

    Factorize out the XSLT code and improve the comments.

--- pkg-java/src/org/expath/pkg/repo/tools/PackagesXmlFile.java
+++ pkg-java/src/org/expath/pkg/repo/tools/PackagesXmlFile.java
@@ -49,90 +49,40 @@ public class PackagesXmlFile
     }
 
     /**
-     * Use a stylesheet to update packages.xml.
-     *
-     * Because we want to write the result back to the same file as the input,
-     * we want to be sure we won't delete it first to read it.  So we buffer
-     * the output.  In order to avoid creating a temporary file, we buffer the
-     * result in memory, by serializing it to a string.  This is ok for such a
-     * small file.
+     * Add a package to packages.xml.
+     * 
+     * @param dir The name of the directory where the package is installed,
+     * right below the repository root.
      */
     public void addPackage(Package pkg, String dir)
             throws PackageException
     {
-        try {
-            // cache the compiled stylesheet?
-            ClassLoader loader = FileSystemStorage.class.getClassLoader();
-            InputStream style_in = loader.getResourceAsStream(ADD_PACKAGE_XSL);
-            if ( style_in == null ) {
-                throw new PackageException("Resource not found: " + ADD_PACKAGE_XSL);
-            }
-            Source style_src = new StreamSource(style_in);
-            style_src.setSystemId(ADD_PACKAGE_XSL);
-            Templates style = TransformerFactory.newInstance().newTemplates(style_src);
-            Source src = new StreamSource(myFile);
-            StringWriter res_out = new StringWriter();
-            Result res = new StreamResult(res_out);
-            Transformer trans = style.newTransformer();
-            trans.setParameter("name",    pkg.getName());
-            trans.setParameter("dir",     dir);
-            trans.setParameter("version", pkg.getVersion());
-            trans.transform(src, res);
-            OutputStream out = new FileOutputStream(myFile);
-            out.write(res_out.getBuffer().toString().getBytes());
-            out.close();
-        }
-        catch ( TransformerConfigurationException ex ) {
-            throw new PackageException("Impossible to compile the stylesheet: " + ADD_PACKAGE_XSL, ex);
-        }
-        catch ( TransformerException ex ) {
-            throw new PackageException("Error transforming packages.xml", ex);
-        }
-        catch ( FileNotFoundException ex ) {
-            throw new PackageException("File not found (wtf? - I just transformed it): " + myFile, ex);
-        }
-        catch ( IOException ex ) {
-            throw new PackageException("Error writing the file: " + myFile, ex);
-        }
+        Transformer trans = compile(ADD_PACKAGE_XSL);
+        trans.setParameter("name",    pkg.getName());
+        trans.setParameter("dir",     dir);
+        trans.setParameter("version", pkg.getVersion());
+        transform(trans);
     }
 
+    /**
+     * Remove a package from packages.xml.
+     * 
+     * The package is identified by its directory name (the name of the
+     * directory where it is installed, right below the repository root).
+     */
     public void removePackageByDir(String dir)
             throws PackageException
     {
-        try {
-            // cache the compiled stylesheet?
-            ClassLoader loader = PackagesXmlFile.class.getClassLoader();
-            InputStream style_in = loader.getResourceAsStream(REMOVE_PACKAGE_XSL);
-            if ( style_in == null ) {
-                throw new PackageException("Resource not found: " + REMOVE_PACKAGE_XSL);
-            }
-            Source style_src = new StreamSource(style_in);
-            style_src.setSystemId(REMOVE_PACKAGE_XSL);
-            Templates style = TransformerFactory.newInstance().newTemplates(style_src);
-            Source src = new StreamSource(myFile);
-            StringWriter res_out = new StringWriter();
-            Result res = new StreamResult(res_out);
-            Transformer trans = style.newTransformer();
-            trans.setParameter("dir", dir);
-            trans.transform(src, res);
-            OutputStream out = new FileOutputStream(myFile);
-            out.write(res_out.getBuffer().toString().getBytes());
-            out.close();
-        }
-        catch ( TransformerConfigurationException ex ) {
-            throw new PackageException("Impossible to compile the stylesheet: " + REMOVE_PACKAGE_XSL, ex);
-        }
-        catch ( TransformerException ex ) {
-            throw new PackageException("Error transforming packages.xml", ex);
-        }
-        catch ( FileNotFoundException ex ) {
-            throw new PackageException("File not found (wtf? - I just transformed it): " + myFile, ex);
-        }
-        catch ( IOException ex ) {
-            throw new PackageException("Error writing the file: " + myFile, ex);
-        }
+        Transformer trans = compile(REMOVE_PACKAGE_XSL);
+        trans.setParameter("dir", dir);
+        transform(trans);
     }
 
+    /**
+     * Create an empty file.
+     * 
+     * By empty, means with the root element, with no package element.
+     */
     private void createIt()
             throws PackageException
     {
@@ -146,6 +96,61 @@ public class PackagesXmlFile
         }
         catch ( IOException ex ) {
             throw new PackageException("Error creating the packages xml list: " + myFile, ex);
+        }
+    }
+
+    /**
+     * Compile a stylesheet.
+     */
+    private Transformer compile(String rsrc_name)
+            throws PackageException
+    {
+        try {
+            // cache the compiled stylesheet?
+            ClassLoader loader = FileSystemStorage.class.getClassLoader();
+            InputStream style_in = loader.getResourceAsStream(rsrc_name);
+            if ( style_in == null ) {
+                throw new PackageException("Resource not found: " + rsrc_name);
+            }
+            Source style_src = new StreamSource(style_in);
+            style_src.setSystemId(rsrc_name);
+            Templates style = TransformerFactory.newInstance().newTemplates(style_src);
+            return style.newTransformer();
+        }
+        catch ( TransformerConfigurationException ex ) {
+            throw new PackageException("Impossible to compile the stylesheet: " + rsrc_name, ex);
+        }
+    }
+
+    /**
+     * Transform the file with the transformer.
+     * 
+     * Because we want to write the result back to the same file as the input,
+     * we want to be sure we won't delete it first to read it.  So we buffer
+     * the output.  In order to avoid creating a temporary file, we buffer the
+     * result in memory, by serializing it to a string.  This is OK for such a
+     * typically small file.
+     */
+    private void transform(Transformer trans)
+            throws PackageException
+    {
+        try {
+            Source src = new StreamSource(myFile);
+            StringWriter res_out = new StringWriter();
+            Result res = new StreamResult(res_out);
+            trans.transform(src, res);
+            OutputStream out = new FileOutputStream(myFile);
+            out.write(res_out.getBuffer().toString().getBytes());
+            out.close();
+        }
+        catch ( TransformerException ex ) {
+            throw new PackageException("Error transforming the file: " + myFile, ex);
+        }
+        catch ( FileNotFoundException ex ) {
+            throw new PackageException("File not found (wtf? - I just transformed it): " + myFile, ex);
+        }
+        catch ( IOException ex ) {
+            throw new PackageException("Error writing the file: " + myFile, ex);
         }
     }
 
