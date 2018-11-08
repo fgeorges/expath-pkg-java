77bd8a052c8565c767e1eccd183781cff2eebff1
Adam Retter <adam.retter@googlemail.com>
2018-11-08 17:39:03 +0800

    [bugfix] Update Java test module for Saxon for Saxon version 9.7.0-15

---
+++ pkg-saxon/src/test/java/com/example/ext/Simple.java
@@ -0,0 +1,7 @@
+package com.example.ext;
+
+public class Simple {
+   public static String hello(String who) {
+      return "Hello, " + who + "!";
+   }
+}
---
+++ pkg-saxon/src/test/java/com/example/ext/SimpleFunction.java
@@ -0,0 +1,39 @@
+package com.example.ext;
+
+import net.sf.saxon.lib.ExtensionFunctionCall;
+import net.sf.saxon.lib.ExtensionFunctionDefinition;
+import net.sf.saxon.om.StructuredQName;
+import net.sf.saxon.type.BuiltInAtomicType;
+import net.sf.saxon.type.ItemType;
+import net.sf.saxon.value.SequenceType;
+
+import static net.sf.saxon.expr.StaticProperty.EXACTLY_ONE;
+
+public class SimpleFunction extends ExtensionFunctionDefinition {
+   private static final String NS_PREFIX = "ext";
+   private static final String NS_URI = "http://www.example.com/ext";
+   private static final String LOCAL_NAME = "hello";
+
+   @Override
+   public StructuredQName getFunctionQName() {
+      return new StructuredQName(NS_PREFIX, NS_URI, LOCAL_NAME);
+   }
+
+   @Override
+   public SequenceType[] getArgumentTypes() {
+      ItemType itype = BuiltInAtomicType.STRING;
+      SequenceType stype = SequenceType.makeSequenceType(itype, EXACTLY_ONE);
+      return new SequenceType[]{stype};
+   }
+
+   @Override
+   public SequenceType getResultType(SequenceType[] params) {
+      ItemType itype = BuiltInAtomicType.STRING;
+      return SequenceType.makeSequenceType(itype, EXACTLY_ONE);
+   }
+
+   @Override
+   public ExtensionFunctionCall makeCallExpression() {
+      return new SimpleCall();
+   }
+}
